f270f1d1643c0dc2d0e09aacc8d34a0c5a122383
Alba Simonsen <99955284+Alivia1620@users.noreply.github.com>
2026-04-29 12:01:21 +0200

    CauldronScript

--- Unity/Wizard_fireball/Assets/Scripts/CauldronColorChange.cs
+++ Unity/Wizard_fireball/Assets/Scripts/CauldronColorChange.cs
@@ -3,17 +3,28 @@ using UnityEngine;
 public class CauldronColorChange : MonoBehaviour
 {
     public Animator animator;
+    public GameObject ingredient;
+
+    void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
         // Check if the object that hit the trigger has the "ingredient" tag
         if (collision.CompareTag("ingredient"))
         {
+            ingredient = collision.gameObject;
             // Play the animation
             if (animator != null)
             {
-                animator.SetTrigger("IngredientAdded");
+                animator.SetBool("IngredientAdded", true);
             }
+            Destroy(ingredient);
         }
     }
 }
